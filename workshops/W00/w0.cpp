47ef51a8c4c6705a2f66f7f6211f1f7d0009ec6b
Abu-ubaida Ali <aali466@myseneca.ca>
2025-05-09 13:48:17 -0400

    workshop 0 finished

---
+++ workshops/W00/w0.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "#### Workshop Zero ####" << endl;
+	cout << "Subject and Section:" << endl;
+	cout << "OOP244 NEE" << endl;
+	cout << "Name:" << endl;
+	cout << "Abu-ubaidah Ali" << endl;
+	cout << "Student Number:" << endl;
+	cout << "138137245" << endl;
+	cout << "Email" << endl;
+	cout << "aali466@myseneca.ca" << endl;
+ 
+	return 0;
+}
